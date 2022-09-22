2a543a6957db52e188ae109e6c01f117393f401d
kanastrax76 <108521251+kanastrax76@users.noreply.github.com>
2022-09-22 22:12:38 +0300

    Update models.py

--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -2,15 +2,45 @@ from django.db import models
 
 
 class Article(models.Model):
-
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение')
 
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30)
+
+    # articles = models.ManyToManyField(Article, related_name='tages')
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Scope(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, verbose_name='Раздел')
+    is_main = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'Тематика статьи'
+        verbose_name_plural = 'Тематики статьи'
+        ordering = ['-is_main', '-tag']
+
+    def __str__(self):
+        return f"{self.article}_{self.tag}"
+
+# ValueError: Cannot alter field articles.Tag.articles into articles.Tag.articles - they are not compatible types (you cannot alter to or from M2M fields, or add or remove through= on M2M fields)
