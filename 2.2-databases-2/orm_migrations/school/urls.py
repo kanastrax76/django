7721867c4c58e044ec313058e5f2889c7332c8a4
kanastrax76 <108521251+kanastrax76@users.noreply.github.com>
2022-09-22 22:14:42 +0300

    Update urls.py

--- 2.2-databases-2/orm_migrations/school/urls.py
+++ 2.2-databases-2/orm_migrations/school/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+import debug_toolbar
+from django.urls import path, include
+from django.contrib import admin
 
 from school.views import students_list
 
 urlpatterns = [
     path('', students_list, name='students'),
-]
+    path('__debug__/', include(debug_toolbar.urls)),
+    path('admin/', admin.site.urls),
+
