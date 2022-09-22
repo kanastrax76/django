4f74e02d0098a7242d2267e89f5d79409d114ab8
kanastrax76 <108521251+kanastrax76@users.noreply.github.com>
2022-09-22 22:12:04 +0300

    Update admin.py

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,39 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Scope, Tag
+
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        its_main = 0
+        for form in self.forms:
+            if self.deleted_forms and self._should_delete_form(form):
+                continue
+            elif form.cleaned_data.get('is_main'):
+                its_main += 1
+        if its_main > 1:
+            raise ValidationError('Основным может быть только один раздел')
+        elif its_main == 0:
+            raise ValidationError('Укажите основной раздел')
+        return super().clean()  # вызываем базовый код переопределяемого метода
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
+    extra = 0
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'title', 'published_at']
+    list_filter = ['title']
+    inlines = [ScopeInline, ]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    # inlines = [ScopeInline, ]
