86afe6d3745cc37cbe0da4f5da6a6debffc12be2
kanastrax76 <108521251+kanastrax76@users.noreply.github.com>
2022-09-22 22:17:43 +0300

    Update urls.py

--- 2.2-databases-2/orm_migrations/website/urls.py
+++ 2.2-databases-2/orm_migrations/website/urls.py
@@ -15,10 +15,8 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
     path('', include('school.urls')),
-    path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
