a5754676387de86d1087d3dc7a8c04b2ba49c1a7
kanastrax76 <108521251+kanastrax76@users.noreply.github.com>
2022-09-22 22:15:47 +0300

    Update views.py

--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -1,12 +1,18 @@
 from django.views.generic import ListView
 from django.shortcuts import render
 
-from .models import Student
+from .models import Student, Teacher
 
 
 def students_list(request):
     template = 'school/students_list.html'
-    context = {}
+    objects_list = Student.objects.all().prefetch_related('teachers')
+    # teachers_list = students_list.teachers.all()
+    context = {
+        'object_list': objects_list,
+        # 'teachers_list': teachers_list
+
+    }
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
