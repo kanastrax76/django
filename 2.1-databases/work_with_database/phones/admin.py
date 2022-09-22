101a4c4a22bf127adf9d72d5f2d8eafdfde722e5
kanastrax76 <108521251+kanastrax76@users.noreply.github.com>
2022-09-22 20:01:42 +0300

    Update admin.py

--- 2.1-databases/work_with_database/phones/admin.py
+++ 2.1-databases/work_with_database/phones/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from phones.models import Phone
+
+
+@admin.register(Phone)
+class PhoneAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'price', 'slug']
+    list_filter = ['name', 'slug']
