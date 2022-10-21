4463b79f16a72fa27bc4272c017455ae1bae17cf
kanastrax76 <108521251+kanastrax76@users.noreply.github.com>
2022-10-21 19:26:46 +0300

    DRF intro is ready

--- 3.1-drf-intro/smart_home/measurement/admin.py
+++ 3.1-drf-intro/smart_home/measurement/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Sensor, Measurement
+
+
+@admin.register(Sensor)
+class SensorAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'description']
+    list_filter = ['name']
+
+
+@admin.register(Measurement)
+class MeasurementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'temperature', 'created_at', 'photo']
+    list_filter = ['id']
