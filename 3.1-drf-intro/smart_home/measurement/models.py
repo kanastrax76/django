ef5ea1c61a22b02b37b886ae95ef95427eda1401
kanastrax76 <108521251+kanastrax76@users.noreply.github.com>
2022-10-21 19:27:23 +0300

    DRF intro is ready

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+
+
+class Sensor(models.Model):
+    name = models.CharField(unique=True, max_length=50)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Measurement(models.Model):
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='measurements')
+    temperature = models.FloatField(null=False, max_length=5)
+    created_at = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(null=True, upload_to='images/')
