f9dbb03f83e4648ac9884d42e31939b8c28a7ea4
kanastrax76 <108521251+kanastrax76@users.noreply.github.com>
2022-10-21 19:28:46 +0300

    DRF intro is ready

--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import ListSensor, GetUpdateDeleteSensor, AddMeasurement, imageForm
 
 urlpatterns = [
-    # TODO: зарегистрируйте необходимые маршруты
+    path('sensors/', ListSensor.as_view()),
+    path('sensors/<int:pk>', GetUpdateDeleteSensor.as_view()),
+    path('measurements/', AddMeasurement.as_view()),
 ]
