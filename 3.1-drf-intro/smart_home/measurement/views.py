4c56bbd7565e4a37814a35e2b76cb4412afb47c6
kanastrax76 <108521251+kanastrax76@users.noreply.github.com>
2022-10-21 19:29:09 +0300

    DRF intro is ready

--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,48 @@
 # TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
 # TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from django.conf import settings
+from django.shortcuts import render
+
+from .models import Sensor, Measurement
+from .serializers import SensorSerializer, MeasurementSerializer, InfoSensorSerializer
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, CreateAPIView
+
+
+class GetUpdateDeleteSensor(RetrieveUpdateDestroyAPIView):
+    """
+    Изменение и удаление датчиков
+    """
+    queryset = Sensor.objects.all()
+    serializer_class = InfoSensorSerializer
+
+    class Meta:
+        verbose_name = 'Получение, изменение и удаление сенсоров'
+
+
+class AddMeasurement(CreateAPIView):
+    """
+    Ввод показаний датчика
+    """
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
+
+
+class ListSensor(ListCreateAPIView):
+    """
+    Список всех датчиков и создание нового датчика
+    """
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+    class Meta:
+        verbose_name = 'Создание сенсора и вывод списка сенсоров'
+
+
+def imageForm(request, image_name):
+    template = 'images.html'
+    measurement = Measurement.objects.get(photo='images/' + image_name)
+    context = {
+        'url': measurement.photo.url,
+        'name': image_name,
+    }
+    return render(request, template, context)
