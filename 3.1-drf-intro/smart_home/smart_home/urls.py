f3ffa913b58ac845dd1a92b1a127d78e04319cf5
kanastrax76 <108521251+kanastrax76@users.noreply.github.com>
2022-10-21 19:30:36 +0300

    DRF intro is ready

--- 3.1-drf-intro/smart_home/smart_home/urls.py
+++ 3.1-drf-intro/smart_home/smart_home/urls.py
@@ -1,5 +1,4 @@
 """smart_home URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
@@ -13,10 +12,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from measurement.views import imageForm
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('measurement.urls')),  # подключаем маршруты из приложения measurement
-]
+    path('', imageForm),
+    path('api/', include('measurement.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
