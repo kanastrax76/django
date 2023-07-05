8faf0674128b695e32f6f4b2f180072b3d568438
kanastrax76 <108521251+kanastrax76@users.noreply.github.com>
2023-07-05 15:06:25 +0300

    finished permissions

---
+++ 3.3-permissions/api_with_restrictions/advertisements/permissions.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from rest_framework import permissions
+
+
+class IsOwnerOrIsStaffOrReadOnly(permissions.BasePermission):
+    """
+    Object-level permission to only allow owners of an object to edit it.
+    Assumes the model instance has an `owner` attribute.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS or request.user.is_staff:
+            return True
+
+        # Instance must have an attribute named `owner`.
+        return obj.creator == request.user
