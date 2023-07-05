293f526cf2929c26c40d81c32796b4405aaacfec
kanastrax76 <108521251+kanastrax76@users.noreply.github.com>
2023-07-05 14:28:23 +0300

    DRF intro is ready

--- 3.2-crud/stocks_products/logistic/admin.py
+++ 3.2-crud/stocks_products/logistic/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.forms import BaseInlineFormSet
 
-# Register your models here.
+from .models import Product, Stock, StockProduct
+
+
+class StockProductInlineFormset(BaseInlineFormSet):
+    pass
+
+
+class StockProductInline(admin.TabularInline):
+    model = StockProduct
+    formset = StockProductInlineFormset
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description']
+
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    list_display = ['id', 'address']
+    inlines = [StockProductInline, ]
+
+
+@admin.register(StockProduct)
+class StockProductAdmin(admin.ModelAdmin):
+    list_display = ['stock', 'product', 'quantity', 'price']
+
