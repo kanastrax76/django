a4ade8ab30a73b99c27f8eeb7f5e9efb3a6c92cb
kanastrax76 <108521251+kanastrax76@users.noreply.github.com>
2023-07-05 15:05:33 +0300

    finished permissions

--- 3.3-permissions/api_with_restrictions/advertisements/models.py
+++ 3.3-permissions/api_with_restrictions/advertisements/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
 
 class AdvertisementStatusChoices(models.TextChoices):
@@ -7,6 +8,7 @@ class AdvertisementStatusChoices(models.TextChoices):
 
     OPEN = "OPEN", "Открыто"
     CLOSED = "CLOSED", "Закрыто"
+    DRAFT = "DRAFT", "Черновик"
 
 
 class Advertisement(models.Model):
@@ -16,15 +18,32 @@ class Advertisement(models.Model):
     description = models.TextField(default='')
     status = models.TextField(
         choices=AdvertisementStatusChoices.choices,
-        default=AdvertisementStatusChoices.OPEN
+        default=AdvertisementStatusChoices.DRAFT
     )
     creator = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
+        related_name='creators',
     )
     created_at = models.DateTimeField(
-        auto_now_add=True
+        default=timezone.now
+        # auto_now_add=True
     )
     updated_at = models.DateTimeField(
         auto_now=True
+    )
+
+    def __str__(self):
+        return f'{self.title} // {self.description}'
+
+
+class FavoriteAdvertisement(models.Model):
+    favorite = models.ForeignKey(
+        Advertisement,
+        on_delete=models.CASCADE,
+        related_name='favorites',
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
     )
