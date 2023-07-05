923181dd033605f91ae416e9b9450141f86eb310
kanastrax76 <108521251+kanastrax76@users.noreply.github.com>
2023-07-05 14:30:14 +0300

    CRUD is ready

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -1,43 +1,52 @@
+# from django.core.validators import MinValueValidator
 from rest_framework import serializers
+from logistic.models import Product, Stock, StockProduct
+
+
+# from rest_framework.exceptions import ValidationError
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для продукта
-    pass
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для позиции продукта на складе
-    pass
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
 
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
-    # настройте сериализатор для склада
+    class Meta:
+        model = Stock
+        fields = ['id', 'address', 'positions']
 
     def create(self, validated_data):
-        # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
-
-        # создаем склад по его параметрам
         stock = super().create(validated_data)
-
-        # здесь вам надо заполнить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
-
+        for position in positions:
+            StockProduct.objects.get_or_create(stock=stock, **position)
         return stock
 
     def update(self, instance, validated_data):
-        # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
-
-        # обновляем склад по его параметрам
         stock = super().update(instance, validated_data)
-
-        # здесь вам надо обновить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
-
+        for position in positions:
+            current_product = StockProduct.objects.filter(stock=stock, product=position['product'])
+            if current_product:
+                sum_quantity = position['quantity'] + current_product[0].quantity
+                adv_price = ((position['price'] * position['quantity'])
+                             + (current_product[0].price * current_product[0].quantity)) / sum_quantity
+                defaults = dict(quantity=sum_quantity, price=adv_price)
+            else:
+                defaults = dict(quantity=position['quantity'], price=position['price'])
+            StockProduct.objects.update_or_create(
+                stock=stock,
+                product=position['product'],
+                defaults=defaults
+            )
         return stock
